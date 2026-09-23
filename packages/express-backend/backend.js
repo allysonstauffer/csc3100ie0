5fa639970966c145ebe31bc15b9a6b63d41284bd
Ally <astauf02@calpoly.edu>
2026-09-23 14:19:14 -0700

    Finished IE4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,57 +1,14 @@
 import express from "express";
 import cors from "cors";
-
-const users = {
-  users_list: [
-    { id: "xyz789", name: "Charlie", job: "Janitor" },
-    { id: "abc123", name: "Mac", job: "Bouncer" },
-    { id: "ppp222", name: "Mac", job: "Professor" },
-    { id: "yat999", name: "Dee", job: "Aspring actress" },
-    { id: "zap555", name: "Dennis", job: "Bartender" }
-  ]
-};
+// ChatGPT-5.6 Terra Light helped me figure out/organize that my
+// mongodb connection is handled in user-service.js
+import userService from "./services/user-service.js";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-// generate id on server
-const generateId = () => {
-  return Math.random().toString();
-};
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const deleteUserById = (id) => {
-  const index = users["users_list"].findIndex(
-    (user) => user["id"] === id
-  );
-  if (index !== -1) {
-    users["users_list"].splice(index, 1);
-    return true;
-  }
-  return false;
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -61,42 +18,45 @@ app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
 
-  if (name !== undefined && job !== undefined) {
-    let result = findUserByNameAndJob(name, job);
-    res.send({ users_list: result });
-  } else if (name !== undefined) {
-    let result = findUserByName(name);
-    res.send({ users_list: result });
-  } else {
-    res.send(users);
-  }
+  userService
+    .getUsers(name, job)
+    .then((result) => res.send({ users_list: result }))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params.id;
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  userService
+    .findUserById(id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(result);
+      }
+    })
+    .catch((error) => res.status(500).send(error));
 });
 
 app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  userToAdd.id = generateId(); // use genid when post request creates user
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  userService
+    .addUser(req.body)
+    .then((result) => res.status(201).send(result))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params.id;
-  const deleted = deleteUserById(id);
-  if (deleted) {
-    res.status(204).send();
-  } else {
-    res.status(404).send("Resource not found.");
-  }
+  userService
+    .removeUser(id)
+    .then((result) => {
+      if (result === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch((error) => res.status(500).send(error));
 });
 
 app.listen(port, () => {
